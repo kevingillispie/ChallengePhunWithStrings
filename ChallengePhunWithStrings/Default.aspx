8c3c4e789427320744fae03d52a2653eb68f906d
Kevin_Gillispie <oakenquill@gmail.com>
2017-09-02 14:47:22 -0700

    init commit

---
+++ ChallengePhunWithStrings/Default.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePhunWithStrings.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            1. Reverse your name:<br />
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="reverseButton1" runat="server" OnClick="reverseButton1_Click" Text="Reverse!" />
+            <br />
+            <br />
+            <asp:Label ID="nameLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <hr />
+        </div>
+        <div>
+            2. Reverse the Order of These Names: Luke, Leia, Han, Chewbacca<br />
+            <br />
+            <asp:Button ID="reverseButton2" runat="server" OnClick="reverseButton2_Click" Text="Reverse!" />
+            <br />
+            <br />
+            <asp:Label ID="herosLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <hr />
+        </div>
+        <div>
+            3. Star Wars heros ASCII art:<br />
+            <br />
+            <asp:Button ID="artButton" runat="server" OnClick="artButton_Click" Text="ART!" />
+            <br />
+            <br />
+            <asp:Label ID="artLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <hr />
+        </div>
+        <div>
+            4. Solve this puzzle:<br />
+            NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.<br />
+            <br />
+            <asp:Button ID="solveButton" runat="server" OnClick="solveButton_Click" Text="Solve!" />
+&nbsp;<br />
+            <br />
+            <asp:Label ID="sentenceLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePhunWithStrings/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void reverseButton1_Click(object sender, EventArgs e)
+        {
+            // 1. Reverse contents of a textbox:
+
+            nameLabel.Text = "";
+            char[] name = nameTextBox.Text.ToCharArray();
+            Array.Reverse(name);
+            for (int i = 0; i < name.Length; i++)
+            {
+                nameLabel.Text += name[i];
+            }
+            nameTextBox.Text = "";
+        }
+
+        string[] heroNames = new String[] { "Luke", "Leia", "Han", "Chewbacca" };
+
+        protected void reverseButton2_Click(object sender, EventArgs e)
+        {
+            // 2. Revers order of elements in an array: 
+
+            string herosListReverse = "";
+            for (int i = heroNames.Length - 1; i >= 0; i--)
+            {
+                herosListReverse += String.Format(heroNames[i] + ", ");
+            }
+            herosLabel.Text = herosListReverse.Remove(herosListReverse.Length - 2, 2);
+        }
+
+        protected void artButton_Click(object sender, EventArgs e)
+        {
+            // 3. Create ASCII art with elements in an array:
+
+            string art = "";
+            for (int i = 0; i < heroNames.Length; i++)
+            {
+                decimal length = (14 - heroNames[i].Length) / 2;
+                Math.Floor(length);
+                for (int j = 0; j < length; j++)
+                {
+                    art += "*";
+                }
+                string output = art + heroNames[i];
+                artLabel.Text += String.Format(output.PadRight(14, '*') + "<br>");
+                art = "";
+            }
+            
+        }
+
+        protected void solveButton_Click(object sender, EventArgs e)
+        {
+            // 4. Edit contents of a string:
+
+            string quote = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+            string removeMe = "remove-me";
+
+            int removeMeIndex = quote.IndexOf(removeMe);
+            quote = quote.Remove(removeMeIndex, removeMe.Length);
+            for (int i = 0; i < quote.Length; i++)
+            {
+                quote = quote.Replace("Z", "T");
+            }
+            char firstLetter = quote[0];
+            quote = quote.Remove(0, 1);
+            sentenceLabel.Text = String.Format(firstLetter + quote.ToLower());
+        }
+    }
+}
